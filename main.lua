8d57b5bbbf43e7ddf03f909df88a1ca8516e65d5
Anakievs <138874493+Anakievs@users.noreply.github.com>
2023-12-23 17:17:51 +0000

    Delete main.lua

--- main.lua
+++
@@ -1,9 +0,0 @@
-local search= require('search')
-
-function slash_search()
-	search:init()
-	search:enter_input_mode()
-	mp.osd_message('/', 600)
-end
-
-mp.add_forced_key_binding('/', 'slash_search', slash_search)
